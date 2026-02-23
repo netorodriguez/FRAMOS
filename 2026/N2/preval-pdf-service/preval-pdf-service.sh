1391d82281b3818de592c29fa677e98bda71202a
Raul Ramos <raul.ramos@SESDIGITAL.LOCAL>
2026-02-23 08:36:10 -0500

    20-02-26

---
+++ 2026/N2/preval-pdf-service/preval-pdf-service.sh
@@ -0,0 +1,41 @@
+#!/bin/sh 
+SERVICE_NAME=Servicio_de_autenticacion_chasqui 
+PATH_TO_JAR=/apli/preevaluador/preval-pdf-service.jar 
+PID_PATH_NAME=/tmp/preval-pdf-service-pid 
+case $1 in 
+start)
+       echo "Starting $SERVICE_NAME ..."
+  if [ ! -f $PID_PATH_NAME ]; then 
+       nohup /apli/java/jdk-21.0.8/bin/java -jar  -Dspring.profiles.active=dev $PATH_TO_JAR >> /logapli/preevaluador/preval-pdf-service.log 2>&1&      
+                  echo $! > $PID_PATH_NAME  
+       echo "$SERVICE_NAME started ..."         
+  else 
+       echo "$SERVICE_NAME is already running ..."
+  fi
+;;
+stop)
+  if [ -f $PID_PATH_NAME ]; then
+         PID=$(cat $PID_PATH_NAME);
+         echo "$SERVICE_NAME stoping ..." 
+         kill $PID;         
+         echo "$SERVICE_NAME stopped ..." 
+         rm $PID_PATH_NAME       
+  else          
+         echo "$SERVICE_NAME is not running ..."   
+  fi    
+;;    
+restart)  
+  if [ -f $PID_PATH_NAME ]; then 
+      PID=$(cat $PID_PATH_NAME);    
+      echo "$SERVICE_NAME stopping ..."; 
+      kill $PID;           
+      echo "$SERVICE_NAME stopped ...";  
+      rm $PID_PATH_NAME     
+      echo "$SERVICE_NAME starting ..."  
+      nohup /apli/java/jdk-21.0.8/bin/java -jar  -Dspring.profiles.active=dev $PATH_TO_JAR >> /logapli/preevaluador/preval-pdf-service.log 2>&1&            
+      echo $! > $PID_PATH_NAME  
+      echo "$SERVICE_NAME started ..."    
+  else           
+      echo "$SERVICE_NAME is not running ..."    
+     fi     ;;
+ esac
